49b2dea9bab2f85b5f7ca241f6c03427bc30c8a9
Nitish Victor <nithishvictor@gmail.com>
2016-11-02 21:47:41 +0530

    Video Modal changes

--- src/app/app.js
+++ src/app/app.js
@@ -15,7 +15,12 @@ angular.module('BlurAdmin', [
   'rzModule',
   'nvd3ChartDirectives',
   'angularSpinner',
-  
+  "com.2fdevs.videogular",
+  "com.2fdevs.videogular.plugins.controls",
+  "com.2fdevs.videogular.plugins.overlayplay",
+  "com.2fdevs.videogular.plugins.poster",
+  "com.2fdevs.videogular.plugins.buffering",
+
   'BlurAdmin.theme',
   'BlurAdmin.pages'
 ])
--- src/app/theme/components/eventStats/modals/VideoModalCtrl.js
+++ src/app/theme/components/eventStats/modals/VideoModalCtrl.js
@@ -6,13 +6,118 @@
     .controller('VideoModalCtrl', VideoModalCtrl);
 
   /** @ngInject */
-  function VideoModalCtrl($scope, $uibModal, dataServices) {
-     var data = dataServices;
-     $scope.currentEvent = dataServices.currentEvent;
-     $scope.raw = "Loading...";
-     data.getEventEmotionResults(data.currentEvent.EventCode, function(result){
-         $scope.raw = result[0].MediaURL;
-     });
+  function VideoModalCtrl($scope, $uibModal, dataServices, $sce) {
+    var data = dataServices;
+    $scope.currentEvent = dataServices.currentEvent;
+    $scope.raw = "Loading...";
+    $scope.mediaUrl = "";
+    $scope.config = { sources: [] };
+    $scope.config.theme = 'bower_components/videogular-themes-default/videogular.css'
+    data.getEventEmotionResults(data.currentEvent.EventCode, function (result) {
+      $scope.raw = result[0].MediaURL;
+      console.log(result[0].MediaURL);
+      var sources = [
+        { src: result[0].MediaURL, type: "video/mp4" },
+      ];
+      $scope.config = { sources: sources };
+      $scope.config.theme = 'bower_components/videogular-themes-default/videogular.css'
+    });
+
+    var updateGraph = function (labels, series, graphData, cb) {
+      $scope.eventlabels = labels;
+      $scope.eventStatsData = series;
+      $scope.eventseries = graphData;
+    }
+
+    $scope.eventlabels = [];
+    $scope.eventStatsData = [];
+    $scope.eventseries = [];
+    $scope.rawGraphData = data.currentEvent.rawGraphData;
+    //console.log(data.currentEvent.rawGraphData);
+
+    var getDataSet = function (time) {
+      var video = $scope.currentItem;
+      var data = $scope.rawGraphData;
+      var labels = [];
+      for (var i = 0; i < data.Anger.length; ++i) {
+        var videotime = data.Anger[i].Start / 100000;
+        //if(videotime<time){
+        labels.push(videotime);
+        // }
+        //if(i==data.Anger.length-1){
+        //  labels.push(videotime);
+        //}
+      }
+
+      var series = [];
+      var graphData = [];
+      for (var key in data) {
+        series.push(key);
+        var tempData = [];
+        for (var i = 0; i < data[key].length; ++i) {
+          var videotime = data[key][i].Start / 10000;
+          if (videotime <= time)
+            tempData.push(data[key][i].AverageFeelingValue * 100);
+        }
+        graphData.push(tempData);
+      }
+      console.log(graphData);
+      if (time != 0)
+        updateGraph(labels, series, graphData);
+    }
+
+    var setGraph = function setGraph(results, cb) {
+      var data = results;
+      var labels = [];
+      var series = [];
+      var graphData = [];
+      var spiderData = [];
+      var spiderSeries = [];
+      console.log(results);
+      try {
+        for (var i = 0; i < data.Anger.length; ++i) {
+          labels.push(data.Anger[i].Start / 10000);
+        }
+        var sliderIntervals = data.Anger.length;
+
+        for (var key in data) {
+          series.push(key);
+          if (key != 'Neutral') {
+            spiderSeries.push(key);
+          }
+          var tempData = [];
+          for (var i = 0; i < data[key].length; ++i) {
+            if (!spiderData[i] && key != 'Neutral') {
+              spiderData[i] = [];
+            }
+            if (key != 'Neutral') {
+              spiderData[i].push(data[key][i].AverageFeelingValue * 100);
+            }
+            tempData.push(data[key][i].AverageFeelingValue * 100);
+          }
+          graphData.push(tempData);
+        }
+       
+        $scope.eventlabels = labels;
+        $scope.eventseries = series;
+        $scope.eventStatsData = graphData;
+        
+      }
+      catch (e) {
+      }
+      finally {
+        if (!labels || labels.length == 0) {
+          $scope.statsStatus = 'Nothing to show here :(';
+        }
+      }
+    };
+
+    setGraph($scope.rawGraphData);
+
+    $scope.onUpdate = function (time, duration) {
+      console.log('test', time);
+      var dataset = getDataSet(time);
+    };
   }
 
 
